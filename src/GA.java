d16fcb3cf04013218f14ee1e93cf874bff58773a
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-03-09 20:56:32 +0100

    Add crossover and mutation rates

--- src/GA.java
+++ src/GA.java
@@ -1,11 +1,17 @@
+import msrcpsp.scheduling.Schedule;
+
 /**
  * Created by GreggJakubiak on 09.03.2017.
  */
 public class GA {
 
     private Population population;
+    private int crossoverRate;
+    private int mutationRate;
 
-    public GA(Population population){
+    public GA(Population population,int crossoverRate, int mutationRate){
         this.population = population;
+        this.crossoverRate = crossoverRate;
+        this.mutationRate = mutationRate;
     }
 }
