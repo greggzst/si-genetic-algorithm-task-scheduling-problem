d0f7bc933b5eb2fef305d39ea24689d3fd7a85ba
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-03-09 21:15:46 +0100

    Add roulette method to GA

--- src/GA.java
+++ src/GA.java
@@ -1,5 +1,9 @@
 import msrcpsp.scheduling.Schedule;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * Created by GreggJakubiak on 09.03.2017.
  */
@@ -13,5 +17,19 @@ public class GA {
         this.population = population;
         this.crossoverRate = crossoverRate;
         this.mutationRate = mutationRate;
+    }
+
+    public Schedule[] roulette(){
+        List<Schedule> selectedIndividuals = new ArrayList<>();
+        int populationSize = population.getPopulationSize();
+        Schedule[] individuals = population.getIndividuals();
+        double[] fitnesses = population.getIndividualFitnesses();
+        Random random = new Random();
+        for(int i = 0; i < populationSize; i++){
+            if(random.nextDouble() >= fitnesses[i]){
+                selectedIndividuals.add(individuals[i]);
+            }
+        }
+        return (Schedule[]) selectedIndividuals.toArray();
     }
 }
