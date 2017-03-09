0b69b1f8796afd404de90be1c111ac9a7ce785a6
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-03-09 20:54:28 +0100

    Add GA class

---
+++ src/GA.java
@@ -0,0 +1,11 @@
+/**
+ * Created by GreggJakubiak on 09.03.2017.
+ */
+public class GA {
+
+    private Population population;
+
+    public GA(Population population){
+        this.population = population;
+    }
+}
