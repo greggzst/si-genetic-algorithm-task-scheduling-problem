604286c91c88c21435270210111d74b3d488cf4e
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-03-10 13:28:55 +0100

    prepare test

--- src/AlgorithmUsage.java
+++ src/AlgorithmUsage.java
@@ -1,3 +1,8 @@
+import msrcpsp.io.MSRCPSPIO;
+import msrcpsp.scheduling.Schedule;
+
+import java.io.IOException;
+
 /**
  * Created by GreggJakubiak on 09.03.2017.
  */
@@ -7,10 +12,18 @@ public class AlgorithmUsage {
     private static final String writeFile = "assets/solutions_small/10_3_5_3.sol";
 
     public static void main(String args[]){
+        MSRCPSPIO reader = new MSRCPSPIO();
         Population pop = new Population(100, definitionFile);
         pop.initializeRandomPopulation();
 
         GA ga = new GA(pop, 100, 0.1,0.01);
+        Schedule schedule = ga.start();
+        // save to a file
+        try {
+            reader.write(schedule, writeFile);
+        } catch (IOException e) {
+            System.out.print("Writing to a file failed");
+        }
 
 
     }
