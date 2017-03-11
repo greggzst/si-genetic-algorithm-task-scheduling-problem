82c1a17cfa6513a04130e23a1bdadb66c044eb3d
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-03-11 11:19:57 +0100

    Change fix method

--- src/msrcpsp/scheduling/Schedule.java
+++ src/msrcpsp/scheduling/Schedule.java
@@ -420,12 +420,17 @@ public class Schedule {
     Task[] tasks = getTasks();
     for(Task first: tasks){
       for(Task second: tasks){
-        //there is conflict when both tasks have the same resource and the colide in time
+        //there is conflict when both tasks have the same resource and they colide at the same time
         if(first.getId() != second.getId() && first.getResourceId() == second.getResourceId()
                 && first.getStart() <= second.getStart() && (first.getStart() + first.getDuration()) > second.getStart()){
-          Resource availableResource = findFirstFreeResource(getCapableResources(second));
-          availableResource.setFinish(second.getStart()+second.getDuration());
-          second.setResourceId(availableResource.getId());
+          List<Resource> capableResources = getCapableResources(second,second.getStart());
+          if(capableResources.size() == 0){
+            second.setStart(first.getStart()+first.getDuration());
+          }else{
+            Resource availableResource = findFirstFreeResource(capableResources);
+            availableResource.setFinish(second.getStart()+second.getDuration());
+            second.setResourceId(availableResource.getId());
+          }
         }
       }
     }
