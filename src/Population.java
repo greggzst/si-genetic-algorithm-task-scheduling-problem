c96e99188e302875e9365926be83abf65466e8fa
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-03-16 20:23:54 +0100

    Change invocation parameters

--- src/Population.java
+++ src/Population.java
@@ -207,6 +207,6 @@ public class Population {
         for(Integer index : sortedIndecies){
             individualRouletteRange[index] = rangeValueMap.get(individualDurations[index]);
         }
-        
+
     }
 }
